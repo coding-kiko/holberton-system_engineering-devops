fc9ed7b2423b68eeec0f6a221153c51a485dbb41
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-13 14:16:15 -0300

    pep8

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -16,7 +16,11 @@ for u in users:
 for task in r:
     if str(task.get("userId")) not in data:
         data[str(task.get("userId"))] = []
-    data[str(task.get("userId"))].append({"task": task.get("title"), "completed": task.get("completed"), "username": usernames.get(str(task.get("userId")))})
+    data[str(task.get("userId"))].append({
+                                         "task": task.get("title"),
+                                         "completed": task.get("completed"),
+                                         "username": usernames.get(str(task.get("userId")))
+                                         })
 
 with open('todo_all_employees.json', 'w') as f:
     json.dump(data, f)
