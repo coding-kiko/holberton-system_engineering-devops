124af06c17078b44e0e88e70c78254574a3c19ce
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-13 13:58:53 -0300

    task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""0. Gather data from an API"""
+
+from sys import argv
+import requests
+import json
+
+r = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+data = {}
+usernames = {}
+
+for u in users:
+    usernames[str(u.get("id"))] = u.get("username")
+
+for task in r:
+    data[str(task.get("userId"))].append({"task": task.get("title"), "completed": task.get("completed"), "username": usernames.get(task.get("userId"))})
+
+with open('todo_all_employees.json', 'w') as f:
+    json.dump(data, f)
