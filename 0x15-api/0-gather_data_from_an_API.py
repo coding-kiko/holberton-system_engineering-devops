e7660843beae599175b548a76593f26e76385068
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-13 17:10:31 -0300

    correcting requirements

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
 """0. Gather data from an API"""
 
-from sys import argv
 import requests
+import sys
 
-id = int(argv[1])
-r = requests.get('https://jsonplaceholder.typicode.com/todos').json()
-users = requests.get('https://jsonplaceholder.typicode.com/users').json()
-done = 0
-tasks = []
-total = 0
+if __name__ == "__main__":
+    id = int(sys.argv[1])
+    r = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    done = 0
+    tasks = []
+    total = 0
 
-for user in users:
-    if user.get("id") == id:
-        name = user.get("name")
-        break
+    for user in users:
+        if user.get("id") == id:
+            name = user.get("name")
+            break
 
-for task in r:
-    if task.get("userId") == id:
-        if task.get("completed"):
-            tasks.append(task.get("title"))
-            done += 1
-        total += 1
+    for task in r:
+        if task.get("userId") == id:
+            if task.get("completed"):
+                tasks.append(task.get("title"))
+                done += 1
+            total += 1
 
-print("Employee {} is done with tasks({}/{}):".format(name, done, total))
-for task in tasks:
-    print("\t {}".format(task))
+    print("Employee {} is done with tasks({}/{}):".format(name, done, total))
+    for task in tasks:
+        print("\t {}".format(task))
