28cd4f5909aefb635a9207296429f45f1bbfaa4e
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-13 17:13:39 -0300

    correcting requirements

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """0. Gather data from an API"""
 
-from sys import argv
-import requests
 import csv
+import requests
+from sys import argv
 
-id = int(argv[1])
-r = requests.get('https://jsonplaceholder.typicode.com/todos').json()
-users = requests.get('https://jsonplaceholder.typicode.com/users').json()
-tasks = []
-filename = argv[1] + ".csv"
+if __name__ == "__main__":
+    id = int(argv[1])
+    r = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    tasks = []
+    filename = argv[1] + ".csv"
 
-for user in users:
-    if user.get("id") == id:
-        username = user.get("username")
-        break
+    for user in users:
+        if user.get("id") == id:
+            username = user.get("username")
+            break
 
-with open(filename, mode='w') as f:
-    w = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-    for task in r:
-        if task.get("userId") == id:
-            w.writerow([argv[1], username, task.get("completed"), task.get("title")])
+    with open(filename, mode='w') as f:
+        w = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        for task in r:
+            if task.get("userId") == id:
+                w.writerow([argv[1], username, task.get("completed"), task.get("title")])
