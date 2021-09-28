579a03e6be792b65aa5673620ebe0c481e0c7de7
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-28 12:15:44 -0300

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''task 0'''
+
+from sys import argv
+import requests
+
+def number_of_subscribers(subreddit):
+    '''get number of subs from subreddit'''
+    url = "https://www.reddit.com/r/{}/about.json".format(argv[2])
+    r = requests.get(url).json()
+    try:
+        subs = r["data"]["subscribers"]
+        return subs
+    except:
+        return 0
