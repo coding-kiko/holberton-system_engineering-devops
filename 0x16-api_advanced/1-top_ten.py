e4103599d59ea005021b6b43161865cc2e495eaa
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-28 13:02:03 -0300

    pep8

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,8 @@ import requests
 def top_ten(subreddit):
     '''get top 10 hottest posts subreddit'''
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    r = requests.get(url, headers={'User-agent': 'your bot 0.1'}, allow_redirects=False)
+    r = requests.get(url, headers={'User-agent': 'your bot 0.1'},
+                     allow_redirects=False)
     if r.status_code == 200:
         r = r.json()
         count = 0
