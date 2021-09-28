81e4e99401e373ac18925a83abb34882ee82321b
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-28 16:28:17 -0300

    s

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -4,21 +4,23 @@
 import requests
 
 
-def count_words(subreddit, word_list, pos=0):
+def count_words(subreddit, word_list, pos=0, dict_count={}):
     '''count word occurences in hot post titles'''
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     r = requests.get(url, headers={'User-agent': 'your bot 0.1'},
                      allow_redirects=False)
-    dict_count={}
     if r.status_code != 200:
         return None
     try:
         for search in word_list:
+            print(search)
             if search.lower() in r.json()['data']['children'][pos]['data']['title'].lower():
+                print(r.json()['data']['children'][pos]['data']['title'].lower())
                 if search not in dict_count:
                     dict_count[search] = 0
                 else:
                     dict_count[search] += 1
+                print(dict_count)
     except IndexError:
         return dict_count
     return (count_words(subreddit, word_list, pos+1))
