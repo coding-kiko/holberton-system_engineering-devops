31ecad8d68eaeb582be4eea7691eb5c3a6f41913
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-28 13:30:17 -0300

    d

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''task 2'''
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], pos=0):
+    '''append hottest posts to hot_list'''
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    r = requests.get(url, headers={'User-agent': 'your bot 0.1'},
+                     allow_redirects=False)
+    print(len(r.json()['data']['children']))
+    """ if r.status_code != 200:
+        return None
+    hot_list.append(r.json()['data']['children'][pos])
+    pos += 1
+    recurse(subreddit, hot_list,)
+ """
