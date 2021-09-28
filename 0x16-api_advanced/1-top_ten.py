d5393b4ed1e79249b400b71c7fcfd2f005e61fe2
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-28 12:52:42 -0300

    allow_redirects==false

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,13 +7,13 @@ import requests
 def top_ten(subreddit):
     '''get top 10 hottest posts subreddit'''
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    r = requests.get(url, headers={'User-agent': 'your bot 0.1'}).json()
+    r = requests.get(url, headers={'User-agent': 'your bot 0.1'}, allow_redirects=False).json()
     if r["data"]["after"]:
         count = 0
         for title in r["data"]["children"]:
             print(title["data"]["title"])
             count += 1
-            if count == 11:
+            if count == 10:
                 break
     else:
         print("None")
