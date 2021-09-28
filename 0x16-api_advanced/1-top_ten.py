52262dcfecd8730633e63588f93caeaa2c9d0908
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-28 12:41:06 -0300

    nn

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,12 +8,12 @@ def top_ten(subreddit):
     '''get top 10 hottest posts subreddit'''
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     r = requests.get(url, headers={'User-agent': 'your bot 0.1'}).json()
-    try:
+    if r["data"]["children"]:
         count = 0
         for title in r["data"]["children"]:
             print(title["data"]["title"])
             count += 1
             if count == 9:
                 break
-    except:
+    else:
         print("None")
