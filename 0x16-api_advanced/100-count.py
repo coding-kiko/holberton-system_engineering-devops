776b7913bf014e7949fc57c2bd4ba033b1614103
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-28 16:51:43 -0300

    s

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -12,7 +12,6 @@ def count_words(subreddit, word_list, pos=0, dict_count={}):
     if r.status_code != 200:
         return None
     try:
-        print(r.json()['data']['children'][pos]['data']['title'])
         for search in word_list:
             if search.lower() in r.json()['data']['children'][pos]['data']['title'].lower().split():
                 if search not in dict_count:
@@ -20,7 +19,7 @@ def count_words(subreddit, word_list, pos=0, dict_count={}):
                 else:
                     dict_count[search] += 1
     except IndexError:
-        for key, value in sorted(dict_count.items(), key=lambda kv: kv[1]['key3'], reverse=True):
+        for key, value in sorted(dict_count, key=dict_count.get, reverse=True):
             print('{}: {}'.format(key, value))
         return
     return (count_words(subreddit, word_list, pos+1))
