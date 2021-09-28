aa75b37dd21ca6f89db9cd3d58a97700d2ebc190
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-28 12:53:54 -0300

    s

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 def number_of_subscribers(subreddit):
     '''get number of subs from subreddit'''
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    r = requests.get(url, headers={'User-agent': 'your bot 0.1'}, allow_redirects=False).json()
+    r = requests.get(url, headers={'User-agent': 'your bot 0.1'}).json()
     try:
         subs = r["data"]["subscribers"]
         return subs
